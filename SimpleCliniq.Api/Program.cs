4462d86310d9d7008e2f69b57bd3028d2d80f3f7
Ubuntu <ubuntu@ip-172-31-41-35.ap-southeast-1.compute.internal>
2024-06-08 09:46:04 +0000

    addDockerConfig

--- SimpleCliniq.Api/Program.cs
+++ SimpleCliniq.Api/Program.cs
@@ -24,13 +24,15 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
